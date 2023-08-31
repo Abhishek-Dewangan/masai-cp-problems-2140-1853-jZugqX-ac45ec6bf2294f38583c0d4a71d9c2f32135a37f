797e56485fbd750a25d3521ff3c2c0fb1d58dfa6
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-08-31 20:35:41 +0530

    All data fetched and show on UI

--- src/Components/Booklet.jsx
+++ src/Components/Booklet.jsx
@@ -1,11 +1,47 @@
-const Booklet = () => {
-  return (
-    <div data-testid="Booklet">
-      {/* create a div with className="welcome-div" here*/}
+import { useState } from 'react';
+import QuestionCard from './QuestionCard';
 
-      <div className="questions-container">
-        {/* Append score and question card components here */}
-      </div>
+const Booklet = () => {
+  const [data, setData] = useState([]);
+  const [startExam, setStartExam] = useState(true);
+  const [score, setScore] = useState(0);
+
+  const getQuestions = () => {
+    fetch(
+      'https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-quiz'
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log(res);
+        setData(res.data);
+      })
+      .catch((error) =>
+        console.log({ message: 'Unable to fetch data', error })
+      );
+  };
+
+  return (
+    <div data-testid='Booklet'>
+      {/* create a div with className="welcome-div" here*/}
+      {startExam ? (
+        <div className='welcome-div'>
+          <h1>To begin the exam, click on the 'Start exam' button below</h1>
+          <button
+            onClick={() => {
+              getQuestions();
+              setStartExam(false);
+            }}>
+            Start Exam
+          </button>
+        </div>
+      ) : (
+        <div className='questions-container'>
+          {/* Append score and question card components here */}
+          <button onClick={() => setStartExam(true)}>End Exam</button>
+          <h3>Score: {score}</h3>
+          {data.length && <QuestionCard data={data} />}
+        </div>
+      )}
     </div>
   );
 };
--- src/Components/QuestionCard.jsx
+++ src/Components/QuestionCard.jsx
@@ -1,10 +1,29 @@
-const QuestionCard = () => {
+const QuestionCard = ({ data }) => {
+  console.log(data);
   return (
-    <div className="question-card">
+    <div className='question-card'>
       {/* add question here */}
+      {data.map((elem) => {
+        return (
+          <div key={elem.id}>
+            <h3>{elem.question}</h3>
 
-      <div className="options"></div>
-      <div className="show-ans"></div>
+            <div className='options'>
+              {elem.options.map((el, i) => {
+                return (
+                  <button
+                    className={
+                      elem.correctOptionIndex == i ? 'bgGreen' : 'bgRed'
+                    }>
+                    {el}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+      <div className='show-ans'></div>
     </div>
   );
 };
